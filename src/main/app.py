da5ef8bb4714126534b8d928d33987fd12fbc84c
Peter Mastnak <petter.mastnak@gmail.com>
2019-02-06 09:33:43 +0100

    Added connection to MongoDB to print contents to HTML page,this is not yet full front-end

--- src/main/app.py
+++ src/main/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
+mongo_object = MongoClient(HOST, PORT)
+db = mongo_object['mydb']
+collection = db['mycollection']
 
 @app.route("/", methods = ['GET','POST'])
 def hello():
@@ -7,6 +12,11 @@ def hello():
     content = request.json # Load JSON data sent with POST request
     print (content)
     return "CI server!"
+
+def pymongo_data_display():
+    my_data = collection.find_one()
+    # Here we need to add some defensive programming in case my_data is None
+    return my_data
 
 if __name__ == '__main__':
     app.run(debug = True, port=8080)
