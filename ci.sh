9bb75d6ce8c440929ae8a9a0bb6ea93e70d76887
Henrik <hglass@kth.se>
2019-02-07 20:50:03 +0100

    Fixes bug where ci.sh could not be called from the parent directory of the repository.

--- ci.sh
+++ ci.sh
@@ -1,12 +1,12 @@
 # static syntax check
-python3 -m py_compile src/main/*.py
+python3 -m py_compile DD2480_group_19_CI/src/main/*.py
 if [ $? -eq 1 ]
 then
 	exit 2
 fi
 
 # run tests
-cd src
+cd DD2480_group_19_CI/src
 python3 -m unittest discover test/
 if [ $? -eq 1 ]
 then
