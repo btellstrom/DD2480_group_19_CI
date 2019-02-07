09a7721f109d0974e3aa63fc3fa9d59c4e18da28
btellstrom <benjamin.tellstrom@gmail.com>
2019-02-07 14:24:27 +0100

    Adds simple configuration file as well as a parser for said configuration file
    * Adds a default config file called ci.ini with many non-specified values. Unable to run without filling in currently.
    
    * Adds the configuration module (?) with some global variables that reads the specified configuration file.
    
    * Adds test_config.py as well as an example config file to test config.py.

---
+++ src/test/test_config.py
@@ -0,0 +1,59 @@
+import unittest
+from main import config
+from os.path import abspath
+
+class test_config(unittest.TestCase):
+
+    """
+    Test so that all variables are initialised properly 
+    from the config-file
+    """
+    def test_init_with_file(self):
+        temp = abspath('test/config_example.ini')
+        config.init(temp)
+
+        # Test the api_token option
+        self.assertTrue(
+            config.api_token == "jibberish"
+        )
+
+        # Tests that the database name is properly initialised
+        self.assertTrue(
+            config.mongo_database_name == "CI-History"
+        )
+
+        # Test that the ip-address of the database is properly initialised
+        self.assertTrue(
+            config.mongo_ip == "192.168.0.1"
+        )
+
+        # Test that the database port is properly initialised
+        self.assertTrue(
+            config.mongo_port == '27017'
+        )
+
+        # Test that the database user is properly initialised
+        self.assertTrue(
+            config.mongo_user == "ci_19"
+        )
+
+        # Test that the database user password is properly initialised
+        self.assertTrue(
+            config.mongo_pass == "password"
+        )
+
+    def test_init_default(self):
+        config.init()
+
+        # Test that api_token is properly initialised as None
+        self.assertTrue(
+            config.api_token == ''
+        )
+
+        # Test that name is properly initialised
+        self.assertTrue(
+            config.mongo_database_name == "CI-History"
+        )
+
+if __name__ == '__main__':
+    unittest.main()
