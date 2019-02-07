665a07708fede7b5b72326060ba730ebd0bd5917
Florian Singer <florian.singer33@gmail.com>
2019-02-07 20:35:03 +0100

    Added main function
    For issue #33

--- src/main/app.py
+++ src/main/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request ,render_template
+from main.history import History
+from os.path import abspath
+from main import config
 app = Flask(__name__)
+
+# Database of builds
+history = None
 
 @app.route("/", methods = ['GET','POST'])
 def hello():
@@ -7,5 +13,12 @@ def hello():
     data = request.get_json(silent=True)# Load JSON data sent with POST request
     return render_template('index.html', data=data)
 
+
+def main():
+    global history
+    config.init()
+    history = History(config.mongo_database_name, config.mongo_ip, config.mongo_port, config.mongo_user, config.mongo_pass)
+
 if __name__ == '__main__':
     app.run(debug = True, port=8080)
+    main()
---
+++ src/test/test.py
@@ -0,0 +1,17 @@
+import unittest
+from os.path import abspath
+import main.app
+
+class test(unittest.TestCase):
+
+    """
+    Tests that an instance of History is created when launching the app.
+    """
+    # <!> Requires that ci.ini is completed. <!>
+    #def test_main(self):
+    #    main.app.main()
+    #    self.assertFalse(main.app.history == None)
+        
+
+if __name__ == '__main__':
+    unittest.main()
