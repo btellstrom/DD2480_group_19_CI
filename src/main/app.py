b0f33cc50d2bb95f1ee4bec38e7f252e5cbdf144
Peter Mastnak <petter.mastnak@gmail.com>
2019-02-07 12:57:59 +0100

    Added a template

--- src/main/app.py
+++ src/main/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request ,render_template
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def hello():
 
-    content = request.json # Load JSON data sent with POST request
-    print (content)
-    return "CI server!"
+    data = request.get_json(silent=True)# Load JSON data sent with POST request
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug = True, port=8080)
