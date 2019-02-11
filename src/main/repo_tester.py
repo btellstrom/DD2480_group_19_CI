de0f611d6d0bf47ecb612e566fb636d49667d2d6
Henrik <hglass@kth.se>
2019-02-11 02:29:12 +0100

    Improves testing on repo_tester. repo_tester now also returns -1 for malformed repositories.

--- src/main/repo_tester.py
+++ src/main/repo_tester.py
@@ -3,11 +3,12 @@ import subprocess
 
 def repo_test(json_payload):                                                        
     """                                                                             
-    Clones repo, runs all tests as specified in ci.sh.                              
+    Clones repo, runs all tests as specified in ci.sh.                             
                                                                             
     + json_payload - parsed json payload                 
     
     + return                                                                         
+        + -1 if ci.sh is malformed (returncode != {0,1,2}) or missing
         + 0 if all tests succeeded                                                    
         + 1 if compilation/static syntax check succeeds but tests fail                
         + 2 if compilation/static syntax check fails                                  
@@ -18,10 +19,21 @@ def repo_test(json_payload):
     clone_url = json_payload["repository"]["clone_url"]
     repo_name = json_payload["repository"]["name"]
                                                                                 
-    # clone repo, switch branch, run ci.sh, remove repo                         
+    # clone repo, switch branch                         
     subprocess.run(["git", "clone", clone_url])                                 
     subprocess.run(["git", "-C", repo_name, "checkout", branch_name])           
+    
+    # Check that ci.sh exists
+    ci_exists = subprocess.run(["test", "-f", "ci.sh"])
+    if ci_exists == 1:
+        return -1
+    
+    # Run ci.sh, remove repo.
     exit_code = subprocess.run(["sh", repo_name + "/ci.sh"]).returncode         
-    subprocess.run(["rm", "-rf", repo_name])                                    
-                                                                                
+    subprocess.run(["rm", "-rf", repo_name])
+
+    # Check that ci.sh returns valid exit code
+    if exit_code < 0 or exit_code > 2:
+        return -1
+
     return exit_code
